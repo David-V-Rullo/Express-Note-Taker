65ad8ef021dac73c4783802b161fc77e6e01b063
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-05 19:19:10 -0400

    Working for the most part. Can't get delete functionality working. I'll get there

--- db/saveData.js
+++ db/saveData.js
@@ -13,20 +13,19 @@ class Save {
     read(){
         return readNote('db/db.json', 'utf8')
     }
-    retreiveNotes() {
-        return this.read()
-            .then(res => {
-                //Creates an empty notes variable
-                let parsedNotes;
-                try {
-                    parsedNotes = [].concat(JSON.parse(res))
-                }
-                catch(err) {
-                    //If the retrive notes function returns no data, then just make parsedNotes an empty array
-                    parsedNotes = []
-                }
-                return parsedNotes
-            })
+
+    async retriveNotes() {
+        const res = await this.read();
+        //Creates an empty notes variable
+        let parsedNotes;
+        try {
+            parsedNotes = [].concat(JSON.parse(res));
+        }
+        catch (err) {
+            //If the retrive notes function returns no data, then just make parsedNotes an empty array
+            parsedNotes = [];
+        }
+        return parsedNotes;
     }
     addNote(note) {
         //Using object restructuring to make title and text variables from the passed in object {note} from the POST request
@@ -37,7 +36,7 @@ class Save {
         //creates the new note object and adds a unique id using uuid
         const newNote = {title, text, id:uuidv4()}
 
-        return this.retreiveNotes()
+        return this.retriveNotes()
             .then(notes => [...notes, newNote])
             .then(updatedNotes => this.write(updatedNotes))
             .then(() => newNote)
